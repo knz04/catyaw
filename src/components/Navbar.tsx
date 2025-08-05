8574ce4aef19b2a512e5128fd4676d052e6c1511
hawyeet <53439203+knz04@users.noreply.github.com>
2025-08-05 21:51:08 +0700

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
