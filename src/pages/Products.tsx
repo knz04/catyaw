b738ec580a6c588f7c46d2521cb545de8e79b082
hawyeet <53439203+knz04@users.noreply.github.com>
2025-08-06 19:25:00 +0700

    add: pagination on cat catalogue

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,27 +10,42 @@ interface Cat {
 
 function Products() {
   const [cats, setCats] = useState<Cat[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
   const limit = 18;
+  const [hasMoreCats, setHasMoreCats] = useState<boolean>(true);
 
   const fetchCats = async () => {
     try {
+      const skip = (currentPage - 1) * limit;
       const response = await fetch(
-        `https://cataas.com/api/cats?limit=${limit}&skip=0`
+        `https://cataas.com/api/cats?limit=${limit}&skip=${skip}`
       );
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data: Cat[] = await response.json();
       setCats(data);
-      console.log(data);
-    } catch (error) {}
+      setHasMoreCats(data.length === limit);
+    } catch (error) {
+      console.error("Failed to fetch cats:", error);
+      setError("Failed to load cat data. Please try again.");
+      setCats([]);
+    }
   };
 
   useEffect(() => {
     fetchCats();
-  }, []);
+  }, [currentPage, limit]);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(1, prevPage - 1));
+  };
 
   return (
     <div className="flex flex-col items-center my-8">
@@ -39,6 +54,23 @@ function Products() {
         {cats.map((cat) => (
           <CatCard key={cat.id} cat={cat} />
         ))}
+      </div>
+      <div className="join mt-10 mb-4 shadow-md">
+        <button
+          className="join-item btn btn-primary"
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+        >
+          «
+        </button>
+        <button className="join-item btn btn-ghost">Page {currentPage}</button>
+        <button
+          className="join-item btn btn-primary"
+          onClick={handleNextPage}
+          disabled={!hasMoreCats}
+        >
+          »
+        </button>
       </div>
     </div>
   );
